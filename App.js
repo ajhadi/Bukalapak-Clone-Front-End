396651d1513cae21ab233640b5201bda17b111c9
fdlnfjrrmdni <fdlnfjrrmdni@gmail.com>
2019-07-10 01:18:53 +0700

    add TabBar

--- App.js
+++ App.js
@@ -12,18 +12,38 @@ import React, {Component} from 'react';
 import {
   createStackNavigator,
   createAppContainer,
+  createBottomTabNavigator
 } from 'react-navigation';
+
+import { Image, StyleSheet } from 'react-native';
 
 import HomeScreen from './src/Screens/Home';
 
 import TopBar from './src/Components/Navigation/TopBar';
 
+
+const TabNavigator = createBottomTabNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: ({ navigation }) => ({
+        title: 'Home',
+        tabBarIcon: ({ focused }) => (
+          focused ?
+            <Image style={styles.icon} source={require('./src/Assets/Icons/abc_btn_radio_to_on_mtrl_015.png')} /> :
+            <Image style={styles.icon} source={require('./src/Assets/Icons/abc_btn_radio_to_on_mtrl_000.png')} />
+        ),
+      }),
+    },
+  }
+);
+
 const StackNavigator = createStackNavigator({
   Home: {
-    screen: HomeScreen,
+    screen: TabNavigator,
     navigationOptions: ({navigation}) => ({
-      header:(<TopBar navigation={navigation} screen={'Home'}/>)
-    }),
+      header: (<TopBar navigation={navigation} screen={'Home'} />),
+    })
   },
   Cart: {
     screen: HomeScreen,
@@ -42,3 +62,11 @@ export default class App extends Component {
     )
   }
 }
+
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 35,
+    height: 35,
+  }
+});
