8cd6ca07aa47d89d10e1047fd57df09db6452400
fdlnfjrrmdni <fdlnfjrrmdni@gmail.com>
2019-07-10 01:40:58 +0700

    add screen

---
+++ src/Screens/BukaMall.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class BukaMallScreen extends Component {
+    render() {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text>BukaMallScreen</Text>
+            </View>
+        );
+    }
+}
+
+export default BukaMallScreen;
