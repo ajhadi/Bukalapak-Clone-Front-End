79ad01169de6dc65606378ca6bb0d324a0381261
M Aftarroziq <aftarr32@gmail.com>
2019-07-11 00:24:36 +0700

    add category menu

--- src/Components/Product/MainMenu.js
+++ src/Components/Product/MainMenu.js
@@ -5,7 +5,8 @@ import {
     View,
     StyleSheet,
     TouchableOpacity,
-    Dimensions
+    Dimensions,
+    ScrollView
 } from 'react-native';
 import Carousel, { Pagination } from 'react-native-snap-carousel';
 
@@ -25,7 +26,15 @@ export default class Menu extends Component{
         <Image style={styles.image} source={{uri: 'https://cdn.pixabay.com/photo/2017/05/02/22/43/mushroom-2279558__340.jpg'}} />,
         <Image style={styles.image} source={{uri: 'https://cdn.pixabay.com/photo/2017/05/18/21/54/tower-bridge-2324875__340.jpg'}} />
     ]
+    // category img
+    images = [
+        {uri: 'https://cdn.pixabay.com/photo/2017/05/19/07/34/teacup-2325722__340.jpg'},
+        {uri: 'https://cdn.pixabay.com/photo/2017/05/02/22/43/mushroom-2279558__340.jpg'},
+        {uri: 'https://cdn.pixabay.com/photo/2017/05/18/21/54/tower-bridge-2324875__340.jpg'},
+        {uri: 'https://cdn.pixabay.com/photo/2017/05/16/21/24/gorilla-2318998__340.jpg'},
+    ]
     render(){
+        const { images } = this;
         return(
             <View>
                 <View style={[styles.container,{flexDirection:'row',paddingHorizontal:15}]}>
@@ -50,24 +59,28 @@ export default class Menu extends Component{
                 {/* Loop Carousel */}
                 <View style={[styles.container,{height:210}]}>
                     <View style={{flex:1,flexDirection:'row'}}>
-                        <Text style={{margin:15,fontSize:16,position:'absolute',fontWeight:'bold'}}>Spesial Untukmu</Text> 
-                        <Text style={{fontSize:13,left:280,margin:15,color:'red'}}>Lihat semua</Text> 
+                        <Text style={{margin:15,fontSize:16,fontWeight:'bold'}}>Spesial Untukmu</Text> 
+                        <TouchableOpacity>
+                            <Text style={{fontSize:13,left:110,margin:15,color:'red'}}>Lihat semua</Text>
+                        </TouchableOpacity>
                     </View>
                     <View style={{flex:3}}>
-                        <Carousel
-                            ref={ ref => this.carouselRef = ref }
-                            data={ this.ads }
-                            renderItem={ ({ item }) => item }
-                            onSnapToItem={ i => this.setState({ activeTab : i }) }
-                            sliderWidth={ width }
-                            itemWidth={ 360 }
-                            slideStyle={{paddingHorizontal:5}}
-                            inactiveSlideOpacity={ 1 }
-                            inactiveSlideScale={ 1 }
-                            loop={true}
-                            autoplay={true}
-                            autoplayInterval={3000}
-                        />
+                        <TouchableOpacity>
+                            <Carousel
+                                ref={ ref => this.carouselRef = ref }
+                                data={ this.ads }
+                                renderItem={ ({ item }) => item }
+                                onSnapToItem={ i => this.setState({ activeTab : i }) }
+                                sliderWidth={ width }
+                                itemWidth={ 360 }
+                                slideStyle={{paddingHorizontal:5}}
+                                inactiveSlideOpacity={ 1 }
+                                inactiveSlideScale={ 1 }
+                                loop={true}
+                                autoplay={true}
+                                autoplayInterval={3000}
+                            />
+                        </TouchableOpacity>
                     </View>   
                 </View>
 
@@ -113,6 +126,36 @@ export default class Menu extends Component{
                         </TouchableOpacity>
                     </View>     
                 </View>
+
+                {/* Category Menu */}
+                <View style={[styles.container,{marginBottom:10,height:270}]}>
+                    <View style={{flex:1,flexDirection:'row'}}>
+                        <Text style={{margin:15,fontSize:16,fontWeight:'bold'}}>Kategori Barang</Text> 
+                        <TouchableOpacity>
+                            <Text style={{fontSize:13,left:110,margin:15,color:'red'}}>Lihat semua</Text> 
+                        </TouchableOpacity>
+                    </View>
+                    <View style={{flex:4}}>
+                        <ScrollView
+                            horizontal
+                            pagingEnabled
+                            showsHorizontalScrollIndicator={false}
+                        >   
+                            <View style={{flexDirection:'row',marginLeft:10,marginRight:10}}>
+                            {images.map(image => (
+                                <View>
+                                    <TouchableOpacity>
+                                        <Image style={{height:90,width:120,borderRadius:10,margin:5}} source={image} />
+                                    </TouchableOpacity>
+                                    <TouchableOpacity>
+                                        <Image style={{height:90,width:120,borderRadius:10,margin:5}} source={image} />
+                                    </TouchableOpacity>
+                                </View>    
+                            ))}
+                            </View>
+                        </ScrollView>
+                    </View>
+                </View >
             </View>
         )
     }
