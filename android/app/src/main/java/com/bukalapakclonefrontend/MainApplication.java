ad5a142b8a4e1839ee008fed30a52243249dae42
M Aftarroziq <aftarr32@gmail.com>
2019-07-12 21:04:46 +0700

    add image picker

--- android/app/src/main/java/com/bukalapakclonefrontend/MainApplication.java
+++ android/app/src/main/java/com/bukalapakclonefrontend/MainApplication.java
@@ -3,6 +3,7 @@ package com.bukalapakclonefrontend;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ImagePickerPackage(),
             new VectorIconsPackage(),
             new LinearGradientPackage(),
             new RNGestureHandlerPackage()
